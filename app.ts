5cc7d792a79607b9110ec732ca0d3fad3055b82b
correalo <82238324+correalo@users.noreply.github.com>
2021-12-15 13:13:29 -0300

    merge Sekkar

--- app.ts
+++ app.ts
@@ -4,11 +4,10 @@ var cookieParser = require("cookie-parser");
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { Surgery } from "./entity/Surgery";
-
+import { router } from "./routes/surgeries";
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var surgeriesRouter = require("./routes/surgeries");
 
 var app = express();
 
@@ -19,30 +18,30 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/surgeries", surgeriesRouter);
+app.use("/surgeries", router);
 
-createConnection()
-  .then(async (connection) => {
-    console.log("Inserting a new surgery into the database...");
-    const surgery = new Surgery(
-      "Jose",
-      new Date(),
-      "ccc",
-      "amil",
-      111.11,
-      "hospital",
-      true
-    );
+// createConnection()
+//   .then(async (connection) => {
+//     console.log("Inserting a new surgery into the database...");
+//     const surgery = new Surgery(
+//       "Jose",
+//       new Date(),
+//       "ccc",
+//       "amil",
+//       111.11,
+//       "hospital",
+//       true
+//     );
 
-    await connection.manager.save(surgery);
-    console.log("Saved a new surgery with id: " + surgery.id);
+//     await connection.manager.save(surgery);
+//     console.log("Saved a new surgery with id: " + surgery.id);
 
-    console.log("Loading surgerys from the database...");
-    const surgeries = await connection.manager.find(Surgery);
-    console.log("Loaded surgerys: ", surgeries);
+//     console.log("Loading surgerys from the database...");
+//     const surgeries = await connection.manager.find(Surgery);
+//     console.log("Loaded surgerys: ", surgeries);
 
-    console.log("Here you can setup and run express/koa/any other framework.");
-  })
-  .catch((error) => console.log(error));
+//     console.log("Here you can setup and run express/koa/any other framework.");
+//   })
+//   .catch((error) => console.log(error));
 
 module.exports = app;
